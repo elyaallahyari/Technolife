d757588607aa2e3bb9e5f25df4a6f9d647459683
Elya <elyaallahyari@gmail.com>
2026-05-04 23:55:14 +0330

    first commit

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Head>
+        <title>فروشگاه تکنولایف</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  )
 }
