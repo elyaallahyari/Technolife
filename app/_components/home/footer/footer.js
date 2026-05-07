5fd8254e6339d60c96a08ed0686c4772caa211ce
Elya <elyaallahyari@gmail.com>
2026-05-07 23:56:46 +0330

    Edit style

--- app/_components/home/footer/footer.js
+++ app/_components/home/footer/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <footer className="flex flex-col items-center justify-center rounded-xl bg-linear-to-r from-[#0079b1] to-[#1b3570]  w-screen  text-white sticky bottom-0 p-5 gap-10">
         <div className="flex flex-row justify-between items-center mt-9 w-full">
           <Image src={Logo} width={174} height={50} alt="Logo of Technolife"></Image>
-          <div className="flex flex-row justify-center items-center bg-white p-2 rounded-xl w-45 h-12 text-[#223c78] gap-2">
+          <div className="flex flex-row justify-center items-center bg-white p-2 rounded-xl w-45 h-12 text-[#223c78] gap-2 cursor-pointer hover:bg-gray-50">
             <FaChevronUp />
             <span>بازگشت به بالا</span>
           </div>
