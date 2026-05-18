89276e35b6b8114c61bf3c3fbbef8f069f44f1dc
Elya <elyaallahyari@gmail.com>
2026-05-18 22:52:44 +0330

    Implement Banner section

---
+++ app/_components/home/main/TopMobileBrands.js
@@ -0,0 +1,10 @@
+import Image01 from '@/public/media/'
+
+export default function TopMobileBrands(){
+
+    return(
+        <div className="flex flex-col w-full ">
+    <h3>برترین‌های موبایل</h3>
+    
+    </div>)
+}
