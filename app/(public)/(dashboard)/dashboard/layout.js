d7a018c75057b2a454a7d6823fc5f419fe673160
Elya <elyaallahyari@gmail.com>
2026-05-20 16:23:34 +0330

    Implement menu sidebar

--- app/(public)/(dashboard)/dashboard/layout.js
+++ app/(public)/(dashboard)/dashboard/layout.js
@@ -1,13 +1,15 @@
-import SidebarMenu from '@/app/_components/dashboard/SidebarMenu'
+import SidebarMenu from '@/app/_components/dash/SidebarMenu'
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className='p-14'>
+    <div className="flex flex-row items-center justify-around p-14 bg-[#fcfeff] w-full h-full gap-7">
       <aside>
         <SidebarMenu />
       </aside>
 
-      <div>{children}</div>
+      <div className="border border-[#f0f0f0] bg-white p-4 mx-auto max-w-screen rounded w-full h-full">
+        {children}
+      </div>
     </div>
   )
 }
