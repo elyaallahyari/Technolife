214557b24a2043aa34fe69040285fafb7dbdb2ea
Elya <elyaallahyari@gmail.com>
2026-05-21 13:01:25 +0330

    Implement static profile section

---
+++ app/(public)/(dashboard)/dashboard/profile/page.js
@@ -0,0 +1,15 @@
+import UserInfo from '@/app/_components/dash/UserInfo'
+import { BiUser } from 'react-icons/bi'
+
+export default async function Profile() {
+  return (
+    <section className="p-9 px-18 flex flex-col items-start justify-center text-[#223c76] gap-8">
+      <div className="flex flex-row items-center gap-2 ">
+        <BiUser size={24} />
+        <p className="font-extrabold text-xl">مشخصات فردی</p>
+      </div>
+
+      <UserInfo />
+    </section>
+  )
+}
