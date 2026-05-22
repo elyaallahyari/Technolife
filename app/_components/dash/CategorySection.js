8bfad1fd284aeee8d82466e2c7973c5826fb6747
Elya <elyaallahyari@gmail.com>
2026-05-22 14:20:57 +0330

    Implement category section CRUD

---
+++ app/_components/dash/CategorySection.js
@@ -0,0 +1,179 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+import { FiEdit3 } from 'react-icons/fi'
+import { RiDeleteBin5Line } from 'react-icons/ri'
+import useSWR from 'swr'
+import useSWRMutation from 'swr/mutation'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+async function CreateBrand(url, { arg }) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(arg)
+  }).then((res) => res.json())
+}
+
+async function EditBrand(url, { arg }) {
+  const { _id, ...data } = arg
+  return fetch(`${url}/${_id}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then((res) => res.json())
+}
+
+async function DeleteBrand(url, { arg }) {
+  return fetch(`${url}/${arg}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  }).then((res) => res.json())
+}
+
+export default function CategorySection() {
+  const [editingItem, setEditingItem] = useState(null)
+  const { data, isLoading, mutate } = useSWR('http://localhost:4000/api/category', fetcher)
+  const { trigger: CreateTrigger, isMutating: isCreateMutating } = useSWRMutation(
+    'http://localhost:4000/api/category',
+    CreateBrand
+  )
+  const { trigger: EditTrigger, isMutating: isEditMutating } = useSWRMutation(
+    'http://localhost:4000/api/category',
+    EditBrand
+  )
+  const { trigger: DeleteTrigger, isMutating: isDeleteMutating } = useSWRMutation(
+    'http://localhost:4000/api/category',
+    DeleteBrand
+  )
+
+  const action = async (formData) => {
+    const payload = {
+      name: formData.get('name'),
+      image: formData.get('image'),
+      en_name: formData.get('en_name')
+    }
+    try {
+      if (editingItem) {
+        await EditTrigger({ _id: editingItem._id, ...payload })
+      } else {
+        await CreateTrigger(payload)
+      }
+      mutate()
+      setEditingItem(null)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleEdit = (item) => {
+    setEditingItem(item)
+    document.getElementById('form')?.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
+  const handleDelete = async (id) => {
+    if (!confirm('آیا مایل به حذف هستید؟')) return
+
+    try {
+      await DeleteTrigger(id)
+      mutate()
+    } catch (error) {
+      alert('خطا در حذف: ' + error.message)
+    }
+  }
+
+  return (
+    <section className="flex flex-col items-center justify-start gap-4">
+      <div className="font-extrabold">
+        {isLoading ? 'دسته‌بندی‌های فروشگاه...' : 'دسته‌بندی‌های فروشگاه'}
+      </div>
+      <div className="flex gap-4 flex-wrap">
+        {data &&
+          data.map((item) => (
+            <div
+              key={item._id}
+              className="flex flex-col items-center justify-center text-sm border border-[#f0f0f0] rounded-xl p-1 bg-[#fcfeff] gap-1"
+            >
+              <Image src={item.image} alt={`${item.name}-icon`} width={50} height={50}></Image>
+              <hr className="text-[#f0f0f0] w-full" />
+              {item.name}
+              <hr className="text-[#f0f0f0] w-full" />
+              {item.en_name}
+              <hr className="text-[#f0f0f0] w-full" />
+              <div className="flex justify-evenly gap-3">
+                <span
+                  className="hover:bg-amber-200 rounded p-1 cursor-pointer"
+                  onClick={() => handleEdit(item)}
+                >
+                  <FiEdit3 />
+                </span>
+                <span
+                  className="hover:bg-red-300 rounded p-1 cursor-pointer"
+                  onClick={() => handleDelete(item._id)}
+                >
+                  <RiDeleteBin5Line />
+                </span>
+              </div>
+            </div>
+          ))}
+      </div>
+      <hr className="text-[#f0f0f0] w-full" />
+      <div className="border border-[#f0f0f0] rounded-xl bg-[#fcfeff]">
+        <form
+          id="form"
+          action={action}
+          className="flex flex-row flex-wrap items-center justify-evenly p-5 gap-5"
+        >
+          <div className="flex gap-2 items-center">
+            <label htmlFor="name">نام دسته‌بندی‌</label>
+            <input
+              type="text"
+              name="name"
+              defaultValue={editingItem?.name || ''}
+              placeholder="عنوان را وارد کنید"
+              className="p-2 rounded-xl border border-[#f0f0f0]"
+            />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <label htmlFor="en_name">نام‌انگلیسی دسته‌بندی‌</label>
+            <input
+              type="text"
+              name="en_name"
+              defaultValue={editingItem?.en_name || ''}
+              placeholder="نام‌انگلیسی را وارد کنید"
+              className="p-2 rounded-xl border border-[#f0f0f0]"
+            />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <label htmlFor="image">تصویر دسته‌بندی‌</label>
+            <input
+              type="text"
+              name="image"
+              defaultValue={editingItem?.image || ''}
+              placeholder=" url تصویر را وارد کنید"
+              className="p-2 rounded-xl border border-[#f0f0f0]"
+            />
+          </div>
+
+          <button type="submit" className="p-2 bg-[#223c76] text-sm text-white rounded-xl">
+            ارسال
+          </button>
+        </form>
+      </div>
+    </section>
+  )
+}
