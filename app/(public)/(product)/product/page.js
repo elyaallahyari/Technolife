52c4e2acfe947bcef923215719a841572bd7f3b1
Elya <elyaallahyari@gmail.com>
2026-05-26 19:15:05 +0330

    Implement product page

--- app/(public)/(product)/product/page.js
+++ app/(public)/(product)/product/page.js
@@ -1,5 +1,10 @@
 import AllProducts from '@/app/_components/sections/product/AllProducts'
 
+export const metadata = {
+  title: 'محصولات فروشگاه تکنولایف',
+  description: 'محصولات فروشگاه اینترنتی تکنولایف'
+}
+
 export default function Product() {
   return (
     <>
