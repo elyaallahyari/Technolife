9b5307fdf9bbf172fca6ff9c2e277d9ec0d1f724
Elya <elyaallahyari@gmail.com>
2026-05-27 15:39:12 +0330

    Edit product style

--- app/_components/sections/product/AllProducts.js
+++ app/_components/sections/product/AllProducts.js
@@ -13,14 +13,54 @@ export default function AllProducts() {
         data.map((item) => (
           <div
             key={item._id}
-            className="flex flex-col items-center justify-center p-2 rounded-lg border border-[#f0f0f0]"
+            className="flex flex-col items-center justify-center p-2 rounded-lg border border-[#f0f0f0] shadow max-w-60 h-full max-h-120 w-full"
           >
-            <Image src={item.media[0]?.url} alt="image" width={206} height={206}></Image>
-            <div> {item.name}</div>
-            <div>{item.price}تومان</div>
-            <div>{item.sale}%</div>
-            <div>{item.category?.name}</div>
-            <div>{item.brand?.name}</div>
+            <Image
+              src={item.media[0]?.url}
+              alt="image"
+              width={206}
+              height={206}
+              className="w-full"
+            ></Image>
+            <div className="px-2 pt-6 text-sm w-full">
+              <div>{item.name}</div>
+              <div className="flex flex-row justify-end pt-2 text-gray-700 text-sm">
+                {item.category?.name}
+              </div>
+            </div>
+            <div className="pt-6 w-full">
+              <div className="flex justify-between px-2 pb-2 w-full">
+                {item.sale > 0 ? (
+                  <div className="flex justify-between w-full">
+                    <div className="bg-red-500 rounded h-4 min-w-6 px-1 text-white text-xs flex justify-center items-center gap-0/5">
+                      <span>%</span>
+                      <span>{item.sale}</span>
+                    </div>
+
+                    <div>
+                      <div className="flex flex-row justify-end items-center gap-1 w-full">
+                        <div>
+                          {(
+                            Number(item.price) -
+                            (Number(item.price) * Number(item.sale)) / 100
+                          ).toLocaleString()}
+                        </div>
+                        <span className="text-sm"> تومان</span>
+                      </div>
+                      <div className="w-full flex flex-row justify-end items-center gap-1 text-gray-400 text-sm">
+                        <del>{item.price.toLocaleString()}</del>
+                        <span className="text-sm"> تومان</span>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="w-full flex flex-row justify-end items-center gap-1">
+                    <span>{item.price.toLocaleString()}</span>
+                    <span className="text-sm"> تومان</span>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         ))}
     </section>
