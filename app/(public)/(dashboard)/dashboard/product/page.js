9907ea15acde5ae7050f1306c3d47d3d46de6383
Elya <elyaallahyari@gmail.com>
2026-05-28 12:31:08 +0330

    Edit style

--- app/(public)/(dashboard)/dashboard/product/page.js
+++ app/(public)/(dashboard)/dashboard/product/page.js
@@ -10,7 +10,7 @@ export default async function Products() {
   return (
     <>
       <div className="p-9 px-18 flex flex-col items-start justify-center text-[#223c76] gap-20 relative">
-        <Link href={'/dashboard/product/add'} className="absolute top-[10%] right-[2%]">
+        <Link href={'/dashboard/product/add'} className="absolute top-2 right-2">
           <Add />
         </Link>
         <ProductSection />
