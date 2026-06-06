418a72eabe83c0a6d3a9a26fd98889c51df9e352
Elya <elyaallahyari@gmail.com>
2026-06-06 20:09:47 +0330

    Implement not-found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 
-export default function NotFound() {
+export const metadata = {
+  title: '404 - Page Not Found',
+  description: 'The page you are looking for does not exist.'
+}
+
+export default async function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen gap-6 border">
       <h2>Not Found</h2>
