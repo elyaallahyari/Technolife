56dab73e550ecda01d30417a48180e9937824597
Elya <elyaallahyari@gmail.com>
2026-06-13 13:28:36 +0330

    Implement SSR rendering

--- app/(protected)/dashboard/profile/page.js
+++ app/(protected)/dashboard/profile/page.js
@@ -5,6 +5,8 @@ export const metadata = {
   title: 'مشخصات فردی - تکنولایف'
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function Profile() {
   return (
     <section className="p-9 px-18 flex flex-col items-start justify-center text-[#223c76] gap-20">
