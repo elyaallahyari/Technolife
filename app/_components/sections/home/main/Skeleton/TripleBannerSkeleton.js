6d9b2e487db2e7ea81fb05c6524a53e5bca4c75c
Elya <elyaallahyari@gmail.com>
2026-07-05 17:32:36 +0330

    Edit style

--- app/_components/sections/home/main/Skeleton/TripleBannerSkeleton.js
+++ app/_components/sections/home/main/Skeleton/TripleBannerSkeleton.js
@@ -4,9 +4,9 @@ export default function TripleBannerSkeleton() {
   return (
     <section className="p-8">
       <div className="flex flex-col lg:flex-row gap-6">
-        <Skeleton className="h-[240px] flex-1" />
-        <Skeleton className="h-[240px] flex-1" />
-        <Skeleton className="h-[240px] flex-1" />
+        <Skeleton className="h-60 flex-1" />
+        <Skeleton className="h-60 flex-1" />
+        <Skeleton className="h-60 flex-1" />
       </div>
     </section>
   )
